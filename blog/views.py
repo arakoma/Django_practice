298a361f4d8c8c425f1194a73e36f61d3fd1c48c
arakoma <arakoma555@gmail.com>
2020-03-18 19:05:32 +0900

    update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    # 現在までにPostされたクエリセットを返す
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('pulished_date')
+    return render(request, 'blog/post_list.html', {'post': posts})
