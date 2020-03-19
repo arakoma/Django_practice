ecbb69bf54679b71aa14d9d3d0d47ac85f1994a4
arakoma <arakoma555@gmail.com>
2020-03-19 15:32:20 +0900

    add 記事詳細ページ

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     # rootURLに'post_list'という名前のviewを割り当てる
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
